183d3d2a276f2cd7148bb7f5f6d62c46bb8aaf20
INDRA\agcortes <agcortes@eservicios.indra.es>
2025-03-06 14:10:37 +0100

    Application properties addition

--- ProductsRA/src/main/java/com/portfolio/srv/utils/ProductMapper.java
+++ ProductsRA/src/main/java/com/portfolio/srv/utils/ProductMapper.java
@@ -3,11 +3,13 @@ package com.portfolio.srv.utils;
 import com.portfolio.api.models.Product;
 import com.portfolio.dao.ProductDao;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
   ProductDao productToDao(Product product);
 
+  @Mapping(source = "inStock", target = "isInStock")
   Product productToDto(ProductDao productDao);
 }
