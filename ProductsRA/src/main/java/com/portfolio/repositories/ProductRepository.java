51ee542e7d71d0f0e6f8e0446b9bdba8455a2194
INDRA\agcortes <agcortes@eservicios.indra.es>
2025-03-13 10:57:59 +0100

    repository annotation

--- ProductsRA/src/main/java/com/portfolio/repositories/ProductRepository.java
+++ ProductsRA/src/main/java/com/portfolio/repositories/ProductRepository.java
@@ -2,9 +2,11 @@ package com.portfolio.repositories;
 
 import com.portfolio.dao.ProductDao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductDao, UUID> {
 
   boolean existsByCode(String code);
