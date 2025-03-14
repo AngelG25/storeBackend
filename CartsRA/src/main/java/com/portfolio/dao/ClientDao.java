ffaf07e475ea46201688d338c06d4b530fc5e168
INDRA\agcortes <agcortes@eservicios.indra.es>
2025-03-14 10:38:45 +0100

    add clientDao to cart rest

--- CartsRA/src/main/java/com/portfolio/dao/ClientDao.java
+++ CartsRA/src/main/java/com/portfolio/dao/ClientDao.java
@@ -3,11 +3,10 @@ package com.portfolio.dao;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -15,36 +14,53 @@ import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name = "Cart")
-public class CartDao {
+@Table(name = "Client")
+public class ClientDao {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "idCart", unique = true)
-  private UUID idCart;
-
-  @Column(name = "client")
+  @Column(name = "idClient", unique = true)
   private UUID idClient;
 
-  @OneToMany(mappedBy = "cartDao", cascade = CascadeType.ALL)
-  private List<ProductDao> products;
+  @Column(name = "iban", unique = true)
+  private String iban;
 
-  @OneToOne
-  @JoinColumn(name = "idClient", referencedColumnName = "idClient", unique = true)
-  private ClientDao client;
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "surname")
+  private String surname;
+
+  @Column(name = "phone", unique = true)
+  private String phone;
+
+  @Column(name = "email", unique = true, updatable = false)
+  private String email;
+
+  @Column(name = "address")
+  private String address;
+
+  @OneToOne(mappedBy = "client", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  private CartDao cartDao;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreationTimestamp
+  @Column(name = "creationDate", updatable = false)
+  private Instant creationDate;
 
   @Temporal(TemporalType.TIMESTAMP)
   @UpdateTimestamp
   @Column(name = "updateDate")
   private Instant updateDate;
+
 }
